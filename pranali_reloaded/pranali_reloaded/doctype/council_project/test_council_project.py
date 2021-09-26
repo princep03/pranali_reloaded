b9ca8dff9cce136cc477e9e31d789e5638c7bd5f
princep03 <pandeyprince128@gmail.com>
2021-09-26 14:01:31 +0530

    On branch develop  Changes to be committed: 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_attendees/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_attendees/council_attendees.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_attendees/council_attendees.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_designation/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_designation/council_designation.js 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_designation/council_designation.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_designation/council_designation.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_designation/test_council_designation.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project/council_project.js 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project/council_project.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project/council_project.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project/test_council_project.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project_chairperson/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project_chairperson/council_project_chairperson.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_project_chairperson/council_project_chairperson.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_team/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_team/council_team.js 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_team/council_team.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_team/council_team.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_team/test_council_team.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/district_council_members/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/district_council_members/district_council_members.js 	new file:   pranali_reloaded/pranali_reloaded/doctype/district_council_members/district_council_members.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/district_council_members/district_council_members.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/district_council_members/test_district_council_members.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/host_club/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/host_club/host_club.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/host_club/host_club.py 	new file:   pranali_reloaded/templates/pages/__pycache__/__init__.py

---
+++ pranali_reloaded/pranali_reloaded/doctype/council_project/test_council_project.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Rtr.Neil Trini Lasrado and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCouncilProject(unittest.TestCase):
+	pass
---
+++ pranali_reloaded/pranali_reloaded/doctype/district_council_members/test_district_council_members.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Rtr.Neil Trini Lasrado and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestDistrictCouncilMembers(unittest.TestCase):
+	pass
