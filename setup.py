fba091ace02635e609c7b51748c0dec46dca3314
Prince Pandey <52704378+princep03@users.noreply.github.com>
2021-10-29 10:42:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='pranali_reloaded',
     version=version,
     description='A reporting system for Rotaract District Organization',
-    author='Rtr. Prince Pandey',
+    author='Rotaract 3141',
     author_email='admin@rotaract3141.org',
     packages=find_packages(),
     zip_safe=False,
