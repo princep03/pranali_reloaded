48514c32265919991c3abe26af229ee66232c864
princep03 <pandeyprince128@gmail.com>
2021-12-24 13:27:16 +0530

    On branch develop  Changes to be committed: 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_application/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.js 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.json 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.py 	new file:   pranali_reloaded/pranali_reloaded/doctype/council_application/test_council_application.py 	new file:   pranali_reloaded/pranali_reloaded/print_format/council_application/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/print_format/council_application/council_application.json 	new file:   pranali_reloaded/pranali_reloaded/web_form/council_application/__init__.py 	new file:   pranali_reloaded/pranali_reloaded/web_form/council_application/council_application.js 	new file:   pranali_reloaded/pranali_reloaded/web_form/council_application/council_application.json 	new file:   pranali_reloaded/pranali_reloaded/web_form/council_application/council_application.py

---
+++ pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Rtr. Prince Pandey and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Council Application', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pranali_reloaded/pranali_reloaded/doctype/council_application/test_council_application.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Rtr. Prince Pandey and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCouncilApplication(unittest.TestCase):
+	pass
