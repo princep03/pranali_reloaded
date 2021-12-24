358458783b8e43fdaa65b970297d0d4234ea7e77
princep03 <pandeyprince128@gmail.com>
2021-12-25 04:32:15 +0530

    On branch develop  Changes to be committed: 	modified:   pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.js 	modified:   pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.json 	modified:   pranali_reloaded/pranali_reloaded/print_format/council_application/council_application.json 	modified:   pranali_reloaded/pranali_reloaded/web_form/council_application/council_application.json

--- pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.js
+++ pranali_reloaded/pranali_reloaded/doctype/council_application/council_application.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2021, Rtr. Prince Pandey and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Council Application', {
-	// refresh: function(frm) {
-
-	// }
-});
+// frappe.ui.form.on('Council Application', 'validate', function(frm){
+// 	frm.set_value("letter_head", "Conquer with Courage");
+// 	})
+	
+// });
