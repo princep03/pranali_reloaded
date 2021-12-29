45e6392d51583a8279f68f6c55c5e2bf560a6576
princep03 <pandeyprince128@gmail.com>
2021-12-30 00:19:14 +0530

    On branch develop  Changes to be committed: 	modified:   pranali_reloaded/pranali_reloaded/doctype/ocv_documents/ocv_document_list.js 	modified:   pranali_reloaded/pranali_reloaded/web_form/council_application/council_application.json 	modified:   pranali_reloaded/templates/includes/login/login.css

--- pranali_reloaded/pranali_reloaded/doctype/ocv_documents/ocv_document_list.js
+++ pranali_reloaded/pranali_reloaded/doctype/ocv_documents/ocv_document_list.js
@@ -2,9 +2,9 @@ frappe.listview_settings['OCV Documents'] = {
     add_fields: ["submission_status"],
 
     get_indicator: function(doc) {
-        if (doc.project_status == "Late") {
+        if (doc.submission_status == "Late") {
             return [__("Late"), "orange", "submission_status,=,Late"];
-        } else if (doc.project_status == "On Time") {
+        } else if (doc.submission_status == "On Time") {
             return [__("On Time"), "green", "submission_status,=,On Time"];
         } 
     }
